3e307109c4f6dffe292d3192178100d61cfea074
Jessica Nolan <dovasy0325@gmail.com>
2022-04-13 10:56:36 +0700

    code uplaod

---
+++ README.md
@@ -0,0 +1,17 @@
+
+## Tech Stack
+
+- OpenZeppelin
+- Truffle / Ganache
+
+## Minting 
+
+Minting happens automatically when the Github Action is triggered manually, for more details check `.github/workflows/mint` 
+
+## 💰 Sponsor this project
+
+Donations in ETH or BTC are welcome 
+
+**ETH** `0x762f70367953aa0fE44C640bBa3FCccCCdC81b01`
+
+**BTC** `bc1qpn2qcdvsuxtpvxv7ec5mv2fycu60pp0ye8tnhx`
---
+++ devops/mint.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const colors = require('colors')
+const fetch = require('node-fetch')
+const { utils, ethers } = require('ethers')
+const NFTT = artifacts.require('NFTT.sol')
+const path = require('path')
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2), { string: ['nfts'] })
+
+const { SERVICE_URL } = process.env
+
+const start = async callback => {
+  try {
+    const objectsToBeMinted = []
+
+    const currentTokens = await (await fetch(`${SERVICE_URL}/token`)).json()
+    const currentIndex = currentTokens.length
+    const AMOUNT = Number(argv.nfts) || 1
+
+    const accounts = () =>
+      new HDWalletProvider({
+        mnemonic: process.env.KEY_MNEMONIC,
+        providerOrUrl: process.env.WALLET_PROVIDER_URL,
+      })
+
+    const FROM = ethers.utils.getAddress(accounts().getAddresses()[0])
+
+    for (let i = currentIndex; i < currentIndex + AMOUNT; i++) {
+      objectsToBeMinted.push(`Robot ${i}`)
+    }
+
+    const mintAssetsOnIPFS = await (
+      await fetch(`${SERVICE_URL}/mint`, {
+        method: 'POST',
+        body: JSON.stringify({ objectsToBeMinted }),
+      })
+    ).json()
+
+    const contract = await NFTT.deployed()
+    const price = '1.5'
+
+    const priceWei = utils.parseEther(price)
+    const ipfsURLs = []
+
+    const mintedTokens = await Promise.all(
+      mintAssetsOnIPFS.map(async token => {
+        ipfsURLs.push({
+          name: token.name,
+          image: `https://ipfs.io/ipfs/${token.path}`,
+          description: `Description for ${token.name}`,
+          external_url: 'https://s2paganini.com/',
+        })
+        return await contract.mintCollectable(FROM, token.path, token.name, priceWei, true, {
+          from: FROM,
+        })
+      })
+    )
+
+    const content = `export const tokenProps = ${JSON.stringify([...currentTokens, ...ipfsURLs])}`
+
+    const file = path.resolve(__dirname, '../', 'db.ts')
+    await fs.writeFileSync(file, content)
+
+    console.log(colors.green(`⚡️ Tokens created: ${colors.white(mintedTokens.length)}`))
+    callback()
+    process.exit(0)
+  } catch (e) {
+    console.log(e)
+    callback(e)
+  }
+}
+
+module.exports = start
